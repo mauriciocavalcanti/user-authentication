11d9d1cee728015f01b9fece5adf307717acbc6a
Maurício Neto <malbuquerqueneto@gmail.com>
2020-03-06 16:19:09 -0300

    guarding routes

--- user-authentication-web/src/main/web/src/app/app-routing.module.ts
+++ user-authentication-web/src/main/web/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from 'src/_helpers/auth.guard';
+import { AppComponent } from './app.component';
 
 
 
 const routes: Routes = [
-  { path: 'user', loadChildren: './user/user.module#UserModule' }
+  { path: '', component: AppComponent, canActivate: [AuthGuard] },
+  { path: 'user', loadChildren: './user/user.module#UserModule', canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
