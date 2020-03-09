c08793f76bd4fcad9ebaebbcf780b70820cf5746
Maurício Neto <malbuquerqueneto@gmail.com>
2020-03-09 03:32:30 -0300

    removing canActivate from user routes

--- user-authentication-web/src/main/web/src/app/user/user-routing.module.ts
+++ user-authentication-web/src/main/web/src/app/user/user-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent} from './home/home.component'
+import { AuthGuard } from 'src/_helpers/auth.guard';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
